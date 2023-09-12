27c38d8b468858127b9b79397c1655042d93dcdf
Chakshu Khanna <chakshukhanna2110@gmail.com>
2023-09-12 16:34:16 +0530

    fraction and compnumber class
    before today idk how to multiply 2 complex number😂

--- OOPS/fraction.cpp
+++ OOPS/fraction.cpp
@@ -12,5 +12,55 @@ class Fraction{
     void print(){
         cout<<this->numerator<<"/"<<this->denominator;
     }
+    void simplify(){
+        int gcd=1;
+        int j=min(this->numerator,this->denominator);
+        for(int i=1;i<=j;i++){
+            if(this->numerator%i==0&&this->denominator%i==0){
+                gcd=i;
+            }
+        }
+        this->numerator=this->numerator/gcd;
+        this->denominator=this->denominator/gcd;
+
+    }
+    void add(Fraction const &f2){
+        // int lcm=denominator*f2.denominator;
+        // int x= lcm/denominator;
+        // int y = lcm/f2.denominator;
+        // int num= x*numerator+(y*f2.denominator);
+        // numerator=num;
+        // denominator=lcm;
+        // simplify();
+
+    int lcm = denominator * f2.denominator;
+    int num1 = lcm / denominator * numerator;
+    int num2 = lcm / f2.denominator * f2.numerator;
+    numerator = num1 + num2;
+    denominator = lcm;
+    simplify();
+
+
+    }
+    void multiply(Fraction const &f2){
+        numerator=numerator*f2.numerator;
+        denominator=denominator*f2.denominator;
+        simplify();
+    }
 
 };
+int main(){
+    Fraction f1(10,2);
+    Fraction f2(15,4);
+
+    f1.add(f2);
+    f1.print();
+    cout<<endl;
+    f2.print();
+    f1.print();
+    f1.multiply(f2);
+    f1.print();
+    f2.print();
+    
+
+}
